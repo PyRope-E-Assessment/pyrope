47063ef0b87960a0e62a715a856f4885452914cc
pbrassel <paul.brassel@htwk-leipzig.de>
2024-08-29 13:27:46 +0200

    FEAT: save source code of parent classes if an exercise is derived and return None if the exercise's source code is not available

--- pyrope/config.py
+++ pyrope/config.py
@@ -143,7 +143,9 @@ summary_items = (
 #   necessary to fix them.
 # 'history':
 #   Eternal log file storing all information necessary for statistical purposes
-#   and learning analytics, such as score dashboards.
+#   and learning analytics, such as score dashboards. NOTE: Exercises which are
+#   not stored in a file, e.g. when developed in a Jupyter notebook cell, will
+#   not be added to the history.
 # 'pyrope':
 #   PyRope's internal system messages for debugging.
 log_dir: str = os.path.join(tempfile.gettempdir(), 'pyrope')
