81033b618fad06b91aee0e71d8f5ffdeaa46b363
pbrassel <paul.brassel@htwk-leipzig.de>
2025-02-20 17:19:32 +0100

    TEST: delete test_solution_values test because there could be a maximal score for an input field defined by the user, so there is no need for a solution and treat_none_manually could be set to False

--- pyrope/tests.py
+++ pyrope/tests.py
@@ -258,24 +258,6 @@ class TestParametrizedExercise(unittest.TestCase):
                         self.fail(f"{pyrope_version} is not a valid version.")
 
     @with_all_pexercises
-    def test_solution_values(self, pexercise):
-        """
-        None is only a valid solution if 'treat_none_manually' is set to True.
-        Otherwise, it is not possible to determine if None is an intended
-        solution or was simply not given. Empty strings are treated the same
-        way as None because they are cast to None.
-        """
-        for name, solution in pexercise.solution.items():
-            if pexercise.ifields[name].treat_none_manually is False:
-                self.assertIsNotNone(
-                    solution,
-                    f"None and empty strings are only valid solutions if "
-                    f"'treat_none_manually' is set to True. Found None or '' "
-                    f" as a/the solution with 'treat_none_manually=False' for "
-                    f"input field {name}."
-                )
-
-    @with_all_pexercises
     def test_score_weights(self, pexercise):
         """
         Scores can be weighted via the key word argument 'weights' of the
