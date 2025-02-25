2ae14bbe063b044a438cb53635519e77e6c294ec
pbrassel <paul.brassel@htwk-leipzig.de>
2025-02-25 12:12:18 +0100

    FIX: include Factorisation in examples

--- pyrope/examples.py
+++ pyrope/examples.py
@@ -87,23 +87,25 @@ class Factor(Exercise):
         return 7
 
 
-# class Factorisation(Exercise):
-#
-#     def parameters(self):
-#         return dict(
-#             p=random.randint(2, 9),
-#             q=random.randint(2, 9),
-#         )
-#
-#     def problem(self, p, q):
-#         return Problem(
-#             fr'{p*q} = <<p_>> $\times$ <<q_>>',
-#             p_=Integer(minimum=2),
-#             q_=Integer(minimum=2),
-#         )
-#
-#     def scores(self, p, q, p_, q_):
-#         return p_ * q_ == p * q
+class Factorisation(Exercise):
+
+    def parameters(self):
+        return dict(
+            p=random.randint(2, 9),
+            q=random.randint(2, 9),
+        )
+
+    def problem(self, p, q):
+        return Problem(
+            fr'{p * q} = <<p_>> $\times$ <<q_>>',
+            p_=Integer(minimum=2),
+            q_=Integer(minimum=2),
+        )
+
+    def scores(self, p, q, p_, q_):
+        if None in {p_, q_}:
+            return 0
+        return p_ * q_ == p * q
 
 
 class FortyTwo(Exercise):
